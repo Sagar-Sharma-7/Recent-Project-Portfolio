71997fc5a26e510bda08e940664db9db466427ad
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-10-04 18:38:01 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+const span = document.querySelector("span");
+const btn_github = document.querySelector(".github");
+
+span.addEventListener("click", ()=>{
+    window.open("https://github.com/Sagar-Sharma-7")
+});
+btn_github.addEventListener("click", () => {
+    window.open("https://github.com/Sagar-Sharma-7")
+})
