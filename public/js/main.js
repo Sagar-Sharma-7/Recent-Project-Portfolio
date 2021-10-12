1bdc56596c02eb26e21af54f9a1b9e84b5253d2d
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-10-12 15:42:35 +0530

    project img click function

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const span = document.querySelector("span");
 const btn_github = document.querySelector(".github");
 const nav = document.querySelector("nav");
 const sticky = nav.offsetTop;
+const img = document.querySelectorAll("img");
 
 span.addEventListener("click", ()=>{
     window.open("https://github.com/Sagar-Sharma-7")
@@ -21,7 +22,18 @@ window.onscroll = function(){
         $("nav").css("text-align", "right")
     }else if(document.body.scrollTop < 100 || document.documentElement.scrollTop < 100){
         $("nav").css("text-align", "left")
-    }
-}
+    };
+};
+
+
+// project click
+img.forEach((pic) => {
+    pic.addEventListener("click", ()=>{
+        $(pic).slideToggle()
+    })
+})
+
+
+
 
 console.log("Under Development!");
