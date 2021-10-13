93dac7508a6b166d1a7bbef2871323652e15f6cb
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-10-13 21:35:56 +0530

    proj detail click display

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const btn_github = document.querySelector(".github");
 const nav = document.querySelector("nav");
 const sticky = nav.offsetTop;
 const img = document.querySelectorAll("img");
-const detail = document.querySelectorAll(".detail")
+const detail = document.querySelectorAll(".detail");
 
 span.addEventListener("click", ()=>{
     window.open("https://github.com/Sagar-Sharma-7")
@@ -13,11 +13,18 @@ btn_github.addEventListener("click", () => {
 });
 
 
+
 // project click
 img.forEach((pic) => {
     pic.addEventListener("click", ()=>{
         $("img").slideDown();
         $(pic).slideUp();
+        const num = pic.title;
+        const info = detail[num];
+        detail.forEach((d) => {
+            $(d).css("display", "none")
+            $(info).css("display", "flex");
+        })
     });
 });
 
