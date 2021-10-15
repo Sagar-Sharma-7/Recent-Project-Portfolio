a578559523a6f211daa4b5d02dcbdd5c0e76030d
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-10-15 08:41:35 +0530

    removed text content from html

--- public/js/text.js
+++ public/js/text.js
@@ -1,6 +1,16 @@
 const my_name = document.querySelector(".orange");
 const developer = document.querySelector(".developer");
+const github = document.querySelector(".github");
+const click_info = document.querySelector(".click_info"); 
+const visit_btn = document.querySelectorAll(".Proj_link");
 
 // text content
 my_name.innerHTML = "Sagar Sharma";
 developer.innerHTML = "I'm a front end web developer.";
+github.innerHTML = "Visit my Github →";
+click_info.innerHTML = "Here are my few projects I've worked on recently. Click on the images to get detail about projects.";
+
+visit_btn.forEach((btn) => {
+    btn.innerHTML = "Visit";
+});
+
